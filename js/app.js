f3a47baccf9b0edbf27fd40baac2ecb91c0fe6e6
Andrew Laino <akl2k7@hotmail.com>
2016-11-16 15:56:01 -0700

    Chang index and finished app.js
    Worked on app.js until completion and tweaked index.html as I did so

--- js/app.js
+++ js/app.js
@@ -4,12 +4,7 @@ var score = 0;
 var words;
 var currentWord = "";
 var blanks = "";
-
-function newGame(){
-	// Randomly generate correct answer from array
-
-	// Create a group of dashes equal to the ammount of letter in correct answer
-}
+var message = $("#message");
 
 function drawStage(){
 	context.moveTo(200, 150);
@@ -23,51 +18,9 @@ function drawStage(){
 	context.stroke();
 }
 
-function drawHead(){
-	context.beginPath();
-	context.arc(200, 170, 20, 0, 2*Math.PI);
-	context.stroke();
-}
 
-function drawBody(){
-	context.moveTo(200, 190);
-	context.lineTo(200, 240);
-	context.stroke();
-}
 
-function drawLeftArm(){
-	context.moveTo(200, 200);
-	context.lineTo(160, 180);
-	context.stroke();
-}
-
-function drawRightArm(){
-	context.moveTo(200, 200);
-	context.lineTo(240, 180);
-	context.stroke();
-}
-
-function drawLeftLeg(){
-	context.moveTo(200, 240);
-	context.lineTo(180, 290);
-	context.stroke();
-}
-
-function drawRightLeg(){
-	context.moveTo(200, 240);
-	context.lineTo(220, 290);
-	context.stroke();
-}
-
-drawStage();
-drawHead();
-drawBody();
-drawLeftArm();
-drawRightArm();
-drawLeftLeg();
-drawRightLeg();
-
-function resetGame(){
+function newGame(){
 	let numOfWords = words.length;
 	let random = Math.random() * numOfWords;
 	currentWord = words[random];
@@ -75,49 +28,50 @@ function resetGame(){
 	for(var i = 0; i < currentWord.length; i++){
 		blanks += "_";
 	}
+	context.clearRect(0,0, canvas.width, canvas.height);
+	drawStage();
 	$("#newGame").prop("disabled", true);
 	$("#guess").val("");
 }
 
 // Call when win condition is fulfilled
 function winGame(){
-
+	message.html("You won! Congratulations!");
 }
 
 // Call when you lost
 function gameOver(){
-
+	message.html("Game Over");
 }
 
 // Check submission
 function checkSubmission(submitted){
 
 	function scoreUp(){
-		let message = $("#message");
-
 		score++;
 		drawLimb();
-
-		// Make sure you don't have a game over
-		if(score < 6)
-			message.html("Wrong answer!");
-		else{
-			message.html("Game Over");
-			gameOver();
-		}
 	}
 
 	// Check if the player guessed the entire word
 	if(submitted.length > 1){
 		if(currentWord === submitted){
-
+			winGame();
 		} else {
 			scoreUp();
 		}
 	} else {
 		// Check the word for the letter submitted
-		if(currentWord.indexOf("submitted") > -1){
+		if(currentWord.indexOf(submitted) > -1){
+			// Change instances of letter in blanks variable
+			let newBlank = [];
+			for(var i = 0; i < currentWord.length; i++){
+				if(currentWord[i] === submitted)
+					newBlank.push(submitted);
+				else 
+					newBlank.push("_");
 
+				blanks = newBlank;
+			}
 		} else {
 			scoreUp();
 		}
@@ -127,19 +81,81 @@ function checkSubmission(submitted){
 // Decide which limb to draw based on score
 function drawLimb(){
 
+	// The functions that manipulate the canvas
+	function drawHead(){
+		context.beginPath();
+		context.arc(200, 170, 20, 0, 2*Math.PI);
+		context.stroke();
+	}
+
+	function drawBody(){
+		context.moveTo(200, 190);
+		context.lineTo(200, 240);
+		context.stroke();
+	}
+
+	function drawLeftArm(){
+		context.moveTo(200, 200);
+		context.lineTo(160, 180);
+		context.stroke();
+	}
+
+	function drawRightArm(){
+		context.moveTo(200, 200);
+		context.lineTo(240, 180);
+		context.stroke();
+	}
+
+	function drawLeftLeg(){
+		context.moveTo(200, 240);
+		context.lineTo(180, 290);
+		context.stroke();
+	}
+
+	function drawRightLeg(){
+		context.moveTo(200, 240);
+		context.lineTo(220, 290);
+		context.stroke();
+	}
+
+	// Run everything through a switch to figure out what limb to draw
+	switch(score){
+		case 1:
+			drawHead();
+			break;
+		case 2:
+			drawBody();
+			break;
+		case 3:
+			drawLeftArm();
+			break;
+		case 4:
+			drawRightArm();
+			break;
+		case 5:
+			drawLeftLeg();
+			break;
+		case 6:
+			drawRightLeg();
+			gameOver();
+	}
 }
 
+
+// Event Code
 $(document).ready(function(){
+	newGame();
 	$.getJSON("json/words.json", function(data){
 		words = [];
 		words = data;
 	});
 
 	$("#submit").on("click", function(){
-
+		let submission = $("#guess").text;
+		checkSubmission(submission);
 	});
 
 	$("#newGame").on("click", function(){
-		resetGame();
+		newGame();
 	});
 });
